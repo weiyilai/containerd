06c23567c0df2ca8c04ee539ae422726c3742a81
Derek McGowan <derek@mcg.dev>
2025-11-19 16:19:14 -0800

    Add new server plugin type use for debug server
    The new server plugin type allows services which have listeners to be
    configured as plugins rather than defined directly in the global
    configuration. This provides more configuration consistency and allows
    containerd to be extended for new types of api handlers.
    
    Signed-off-by: Derek McGowan <derek@mcg.dev>

--- plugins/types.go
+++ plugins/types.go
@@ -77,6 +77,8 @@ const (
 	ShimPlugin plugin.Type = "io.containerd.shim.v1"
 	// HTTPHandler implements an http handler
 	HTTPHandler plugin.Type = "io.containerd.http.v1"
+	// ServerPlugin implements a server that starts with the main process, e.g. API listeners
+	ServerPlugin plugin.Type = "io.containerd.server.v1"
 	// MountManagerPlugin implements the mount manager interface
 	MountManagerPlugin plugin.Type = "io.containerd.mount-manager.v1"
 	// MountHandlerPlugin implements the mount handler interface
